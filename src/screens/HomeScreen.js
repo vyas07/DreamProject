2362f4728691e6ba86ee963a115b1c4d38ff23cb
vyas07 <vedavyas683@gmail.com>
2024-03-14 21:24:11 +0530

    HomeScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,19 +1,28 @@
-
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, TouchableOpacity, View , Image } from 'react-native';
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
-import Animated, { useSharedValue } from 'react-native-reanimated';
+import Animated, { useSharedValue, withSpring } from 'react-native-reanimated';
+import { useNavigation }from '@react-navigation/native';
 
-import * as React from 'react';
+
 
 export default function HomeScreen(){
+
+const Navigation = useNavigation();
+
+useEffect(() =>{
+setTimeout(()=> Navigation.navigate('Profile'),5000)
+
+},[])
+
   return(
-    <View className="flex-1 justify-center items-center bg-purple-500 space-y-5">
-          <View className="bg-white/20  rounded-full shadow-md shadow-gray-400" style={{padding: hp(5)}}>
+    <View className="flex-1 justify-center items-center bg-blue-900 space-y-5">
+          <View className="bg-white/60  rounded-full shadow-md shadow-gray-400" style={{padding: hp(5)}}>
             <Image source={require('../../images/boutique.png')} style = {{width:hp(20),height: hp(20)}}/>
           </View>
           <View className="flex items-center space-y-5">
-          <Text className="font-bold text-white tracking-widest text-4xl">BoutiqueIT</Text>
-          <Text className="font-medium text-white tracking-widest text-lg">Fashion never goes out of style</Text>
+          <Text className="font-bold text-white tracking-widest text-4xl" style={{fontStyle: 'italic'}}>BoutiqueIT</Text>
+          <Text className="font-medium text-white tracking-widest text-lg" style={{fontStyle: 'italic'}}>Fashion never goes out of style</Text>
           </View>
         </View>
       );
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,11 +1,46 @@
 
-import {View , Text} from 'react-native'
+import {View , Text , StatusBar , ScrollView, Image , TextInput} from 'react-native'
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import * as React from 'react';
+import { BellIcon , MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+
 
 export default function ProfileScreen(){
   return(
-     <View>
-     <Text>WelcomeScreen</Text>
-      </View>
+      <View className="flex-1 bg-white">
+               <StatusBar style="dark"/>
+               <ScrollView showsVerticalIndicator={false}
+               contentContainerStyle={{paddingBottom:50}}
+               className="space-y-6 pt-14" >
+               <View className="mx-2 flex-row justify-between item-center mb-2">
+                       <Image source={require('../../images/icon.png')} style = {{width:hp(5.5),height: hp(5)}}/>
+                       <BellIcon size={hp(4)} color="gray" />
+                </View>
+
+                <View className="mx-2 space-y-2 mb-2">
+
+                <View className="mx-2 flex-row justify-between item-center mb-2">
+                      <Text style={{fontSize: hp(1.7)}} className="text-neutral-600 font-bold">Hello, Poornima!</Text>
+                 </View>
+
+                  <View className="mx-2 flex-row justify-between item-center mb-2">
+                       <Text style={{fontSize: hp(3)}} className="text-neutral-600 font-semibold">Elevate your style with <Text className="text-amber-400">US</Text></Text>
+                  </View>
+
+                   <View className="mx-2 flex-row justify-between item-center mb-2">
+                       <Text style={{fontSize: hp(3)}} className="text-neutral-600 font-semibold">From your <Text className="text-amber-400">Home</Text></Text>
+                    </View>
+
+                    <View className="mx-1 flex-row item-center rounded-full bg-black/5 p-[6px]">
+                          <TextInput placeholder ='Search Boutique' placeholderTextColor={"gray"}
+                          style={{fontSize: hp(1.7)}}
+                          className="flex-1 text-base mb-1 pl3 tracking-wider"/>
+                    <View className="bg-white rounded-full p-3">
+                          <MagnifyingGlassIcon size={hp(2.5)} strokeWidth={3} color="gray" />
+                    </View>
+                  </View>
+                </View>
+               </ScrollView>
+             </View>
     );
 };
